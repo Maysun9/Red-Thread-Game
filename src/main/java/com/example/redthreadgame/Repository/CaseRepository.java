485fe2154da204e360d817c8588c060ed0f43a84
Maysun Alharbi <maysun.alharbii@gmail.com>
2026-06-03 00:52:25 +0300

    - local save

--- src/main/java/com/example/redthreadgame/Repository/CaseRepository.java
+++ src/main/java/com/example/redthreadgame/Repository/CaseRepository.java
@@ -1,4 +1,19 @@
 package com.example.redthreadgame.Repository;
 
-public class CaseRepository {
+import com.example.redthreadgame.Model.Case;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+
+public interface CaseRepository extends JpaRepository<Case, Integer> {
+    Case findCaseById(Integer id);
+
+    List<Case> findCasesByStatus(String status);
+
+
+    List<Case> findCasesByAdminId(Integer adminId);
+
 }
