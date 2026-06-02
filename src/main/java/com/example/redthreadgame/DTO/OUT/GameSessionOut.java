d561fac44905251b06ec53695543e2b8c863482e
Alhanouf Allazzam <115586263+AlhanoufKMLz@users.noreply.github.com>
2026-06-02 19:24:45 +0300

    feat: game session dto

---
+++ src/main/java/com/example/redthreadgame/DTO/OUT/GameSessionOut.java
@@ -0,0 +1,23 @@
+package com.example.redthreadgame.DTO.OUT;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class GameSessionOut {
+
+    private Integer id;
+    private String status;
+    private Boolean isPrivate;
+    private Integer playersCount;
+    private Integer questionsCount;
+    private Integer score;
+    private LocalDateTime startedAt;
+    private LocalDateTime endedAt;
+}
