5292cce716756fa718d2d1b1d9170cb8c00e5ccb
Alhanouf Allazzam <115586263+AlhanoufKMLz@users.noreply.github.com>
2026-06-02 14:40:03 +0300

    feat: basic structure

---
+++ src/main/java/com/example/redthreadgame/RedThreadGameApplication.java
@@ -0,0 +1,13 @@
+package com.example.redthreadgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedThreadGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedThreadGameApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/redthreadgame/RedThreadGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.redthreadgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedThreadGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
