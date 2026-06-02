cf1ed186d1717a4e580d82ad4e780c7b74ff7560
Alhanouf Allazzam <115586263+AlhanoufKMLz@users.noreply.github.com>
2026-06-02 21:10:54 +0300

    feat: invitation dto

---
+++ src/main/java/com/example/redthreadgame/DTO/OUT/InvitationOut.java
@@ -0,0 +1,15 @@
+package com.example.redthreadgame.DTO.OUT;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class InvitationOut {
+
+    private Integer id;
+    private String status;
+}
